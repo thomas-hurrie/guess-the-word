ce590327b9c12e6e5de94a0ea73998cf731523c4
Thomas Hurrie <thomas.hurrie@gmail.com>
2021-06-12 13:30:49 -0400

    Check validate capture input

--- js/script.js
+++ js/script.js
@@ -11,12 +11,13 @@ const remainingGuesses = document.querySelector(".remaining");
 //span inside paragraph where remaining guesses will appear
 const remainingSpan = document.querySelector(".remaining span");
 //empty paragraph where messages will appear when player guesses letter
-const guessedLetters = document.querySelector("message");
+const guessMessage = document.querySelector(".message");
 //hidden button prompting player to play again
-const hiddenButton = document.querySelector("play-again");
+const hiddenButton = document.querySelector(".play-again");
 
 //starter word
 const word = "magnolia";
+guessedLetters = [];
 
 const placeHolders = function (word) {
     const placeHoldersLetters = [];
@@ -31,7 +32,40 @@ placeHolders(word);
 
 guessButton.addEventListener("click", function(e) {
     e.preventDefault();
+    //empty message paragraph
+    guessMessage.innerText = "";
+    //grab what was entered in input
     const inputLetter = inputGuess.value;
-    console.log(inputLetter);
+    //check input 
+    const validGuess = validateInput(inputLetter);
+
+    if (validGuess) {
+        //make a guess
+        makeGuess(inputLetter);
+    }
     inputGuess.value = "";
 });
+
+//validate player's input
+const validateInput = function(input) {
+    const acceptedLetter = /[a-zA-z]/;
+    if (input.length === 0) {
+    guessMessage.innerText = "Invisible letters don't work!";
+    } else if (input.length > 1) {
+    guessMessage.innerText = "Easy there, one letter at time!";
+    } else if (!input.match(acceptedLetter)) {
+    guessMessage.innerText = "Guess must be a single letter A-Z!";
+    } else {
+    return input;
+    } 
+};
+
+const makeGuess = function (inputLetter) {
+    inputLetter = inputLetter.toUpperCase();
+    if (guessedLetters.includes(inputLetter)) {
+        guessMessage.innerText = "You've already guessed that letter, try again!";
+    } else {
+        guessedLetters.push(inputLetter);
+        console.log(guessedLetters);
+    }
+};
